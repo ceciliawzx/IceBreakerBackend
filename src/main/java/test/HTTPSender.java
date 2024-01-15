0a879713c8e9381b7ad49f055a60ac09685ad073
Leo Li <ll3221@ic.ac.uk>
2024-01-15 16:03:59 +0000

    Custom request message

--- src/main/java/test/HTTPSender.java
+++ src/main/java/test/HTTPSender.java
@@ -2,8 +2,10 @@ package test;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class HTTPSender {
     public static void main(String[] args) {
@@ -11,22 +13,34 @@ public class HTTPSender {
             URL url = new URL("http://ljthey.co.uk:8000");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-            // Setting the request method
-            con.setRequestMethod("GET");
+            // Setting the request method to POST
+            con.setRequestMethod("POST");
 
-            // Reading the response
+            // Enabling both input and output streams
+            con.setDoOutput(true);
+
+            // Custom message to send in the request body
+            String message = "This is a message from Leo Li";
+
+            // Sending the request
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = message.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            // Reading the response code
             int status = con.getResponseCode();
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            content.append("This is Leo Li");
-//            while ((inputLine = in.readLine()) != null) {
-//                content.append(inputLine);
-//            }
-            in.close();
+            System.out.println("Response Code: " + status);
 
-            // Print the response
-            System.out.println(content.toString());
+            // Reading the response message
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println("Response Message: " + response.toString());
+            }
 
             // Closing the connection
             con.disconnect();

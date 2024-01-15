89d9e12dadd09edd00f4ed186a24bde05f71f071
Selina Wan <wansiyu86@hotmail.com>
2024-01-15 17:33:13 +0000

    Creating fields for Admin, Person, User classes

---
+++ src/main/java/com/icebreaker/person/Person.java
@@ -0,0 +1,31 @@
+package com.icebreaker.person;
+
+import lombok.*;
+
+import java.net.InetSocketAddress;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class Person {
+    @NonNull
+    protected InetSocketAddress address;
+    @NonNull
+    protected int id;
+    @NonNull
+    protected byte[] profileImage;
+    @NonNull
+    protected String firstName;
+    @NonNull
+    protected String lastName;
+    @NonNull
+    protected String country;
+    @NonNull
+    protected String city;
+    @NonNull
+    protected String feeling;
+    @NonNull
+    protected String favFood;
+    @NonNull
+    protected String favActivities;
+}

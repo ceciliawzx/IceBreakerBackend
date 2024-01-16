9de3f3c8384b20a1d329db3b581e1d71aac0697f
Leo Li <ll3221@ic.ac.uk>
2024-01-16 13:09:17 +0000

    Added concurrency Test Case

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,3 +1,4 @@
+import com.icebreaker.httprequests.HttpRequests;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,5 +10,32 @@ public class TestTest {
         int expectedResult = 8;
 
         Assert.assertEquals(expectedResult, a+b);
+    }
+
+    @Test
+    public void testRoomCreationConcurrent() throws InterruptedException {
+        final int TOTAL_NUM_OF_THREADS = 20;
+        final int TOTAL_NUM_OF_CALLS = 10000000;
+        final int TOTAL_NUM_OF_CALLS_PER_THREAD = TOTAL_NUM_OF_CALLS / TOTAL_NUM_OF_THREADS;
+
+        HttpRequests httpRequests = new HttpRequests();
+
+        Thread[] threads = new Thread[TOTAL_NUM_OF_THREADS];
+
+        for (int i = 0; i < TOTAL_NUM_OF_THREADS; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < TOTAL_NUM_OF_CALLS_PER_THREAD; j++) {
+                    httpRequests.handleRoomCreation("");
+                }
+            });
+            threads[i].start();
+        }
+
+        for (int i = 0; i < TOTAL_NUM_OF_THREADS; i++) {
+            threads[i].join();
+        }
+
+        Assert.assertEquals(httpRequests.handleRoomCreation(""),
+                "Room Created!!! Your New Room Number is " + TOTAL_NUM_OF_CALLS);
     }
 }

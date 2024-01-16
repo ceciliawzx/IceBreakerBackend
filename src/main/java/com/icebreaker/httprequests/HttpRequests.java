23d3d1f2e2da48a67a6e3a15a1a516c706eb8f85
Leo Li <ll3221@ic.ac.uk>
2024-01-16 13:28:56 +0000

    Add start room and handle join room and get header

--- src/main/java/com/icebreaker/httprequests/HttpRequests.java
+++ src/main/java/com/icebreaker/httprequests/HttpRequests.java
@@ -3,6 +3,7 @@ package com.icebreaker.httprequests;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -20,5 +21,12 @@ public class HttpRequests {
     public String handleRoomCreation(@RequestParam(name = "message", required = false) String message) {
         int newRoomNumber = roomNumber.getAndIncrement();
         return "Room Created!!! Your New Room Number is " + newRoomNumber;
+    }
+
+    @GetMapping("/joinRoom")
+    public String handleJoinRoom(@RequestParam(name = "roomNumber", required = true) int number,
+                                 @RequestHeader(value = "X-Forwarded-For", required = false) String xForwardedFor) {
+        String clientIpAddress = xForwardedFor != null ? xForwardedFor.split(",")[0] : "Unknown";
+        return "You have joined room " + number + ". Your IP address is " + xForwardedFor;
     }
 }

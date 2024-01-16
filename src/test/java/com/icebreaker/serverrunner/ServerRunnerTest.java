675c2eca44ede83e81d44bea71f92a71d0d71692
Cecilia <ceciliawzx@qq.com>
2024-01-16 23:01:28 +0000

    [zw4021] changed some structures of ServerRunner, added unit tests for it

---
+++ src/test/java/com/icebreaker/serverrunner/ServerRunnerTest.java
@@ -0,0 +1,44 @@
+package com.icebreaker.serverrunner;
+
+import static org.junit.Assert.*;
+
+import com.icebreaker.httprequests.HttpRequests;
+import com.icebreaker.room.Room;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+
+public class ServerRunnerTest {
+
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
+    private final ServerRunner serverRunner = ServerRunner.getInstance();
+    private final HttpServletRequest request = context.mock(HttpServletRequest.class);
+
+    @Test
+    public void serverRunnerOnlyCreatesOneInstance() {
+        ServerRunner instance2 = ServerRunner.getInstance();
+        assertSame(serverRunner, instance2);
+    }
+
+    @Test
+    public void serverRunnerCanAddAndDestroyRoom() {
+        Room room = new Room(123, request);
+        assertTrue(serverRunner.addRoom(room));
+        assertTrue(serverRunner.containsRoom(room));
+        assertTrue(serverRunner.containsRoom(123));
+        assertTrue(serverRunner.destroyRoom(123));
+        assertFalse(serverRunner.containsRoom(room));
+        assertFalse(serverRunner.containsRoom(123));
+    }
+
+
+
+}

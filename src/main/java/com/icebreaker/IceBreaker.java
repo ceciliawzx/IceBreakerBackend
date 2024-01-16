a14f69a6cfc88581039a20651294c428d677a6a4
Leo Li <ll3221@ic.ac.uk>
2024-01-16 10:42:29 +0000

    Use spring instead of http directly

--- src/main/java/com/icebreaker/IceBreaker.java
+++ src/main/java/com/icebreaker/IceBreaker.java
@@ -1,44 +1,24 @@
 package com.icebreaker;
 
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class IceBreaker {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!!!");
-        // Create an HTTP server listening on port 8000
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-
-        // Create a context for the root URI ("/")
-        server.createContext("/", new MyHandler());
-
-        // Start the server
-        server.start();
+        SpringApplication.run(IceBreaker.class, args);
     }
 
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            exchange.getRemoteAddress();
-            System.out.println("Hello!!!");
-            // Print request body
-            System.out.println("Request Body: ");
-            InputStream requestBody = exchange.getRequestBody();
-            String body = new Scanner(requestBody, StandardCharsets.UTF_8.name()).useDelimiter("\\A").next();
-            System.out.println(body);
-            String response = "Hello, this is the response";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
+    @GetMapping("/myEndpoint")
+    public String handleRequest(@RequestParam(name = "message", required = false) String message) {
+        System.out.println("YOYOYOYOYOYO");
+        return "Received message: " + message;
     }
 }

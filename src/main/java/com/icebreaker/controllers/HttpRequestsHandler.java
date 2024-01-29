afd543bb82ca22209225e8f99c4dec8dfb95d5f2
Leo Li <ll3221@ic.ac.uk>
2024-01-29 14:05:42 +0000

    Revert to original Leo version of get and increment

--- src/main/java/com/icebreaker/controllers/HttpRequestsHandler.java
+++ src/main/java/com/icebreaker/controllers/HttpRequestsHandler.java
@@ -43,7 +43,8 @@ public class HttpRequestsHandler {
             throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
 
-        int newRoomNumberInt = Integer.parseInt(String.valueOf(roomNumber.getAndIncrement()));
+        int newRoomNumberInt = roomNumber.getAndIncrement();
+
         System.out.println("Room number: " + newRoomNumberInt);
         System.out.println("Atomic Value: " + roomNumber.get());
         String newRoomNumber = String.valueOf(newRoomNumberInt);

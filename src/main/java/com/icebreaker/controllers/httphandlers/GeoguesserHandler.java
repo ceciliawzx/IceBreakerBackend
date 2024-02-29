d6d48d16ab5ea7c9c82c03b5566c310e43736b53
Selina Wan <wansiyu86@hotmail.com>
2024-02-29 13:18:45 +0000

    ICEBREAKER-180: add service

--- src/main/java/com/icebreaker/controllers/httphandlers/GeoguesserHandler.java
+++ src/main/java/com/icebreaker/controllers/httphandlers/GeoguesserHandler.java
@@ -3,6 +3,7 @@ package com.icebreaker.controllers.httphandlers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.icebreaker.room.RoomStatus;
 import com.icebreaker.serverrunner.ServerRunner;
+import com.icebreaker.services.WaitRoomService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,16 @@ import java.util.Map;
 
 public class GeoguesserHandler {
     private final ServerRunner runner = ServerRunner.getInstance();
+    private final WaitRoomService waitRoomService;
+
+    public GeoguesserHandler(WaitRoomService waitRoomService) {
+        this.waitRoomService = waitRoomService;
+    }
 
     @PostMapping("startGeoguesser")
     public boolean startGeoguesser(@RequestParam(name = "roomCode", required = true) String roomCode) {
         if (runner.changeRoomStatus(roomCode, RoomStatus.GEO_GUESSING)) {
+            waitRoomService.broadcastMessage(roomCode);
             return true;
         }
         return false;
